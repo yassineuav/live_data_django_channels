c4e02955e36987ceae95ff119ec7077a5270b0bc
Yassine Khiadouch <yassine.uav@gmail.com>
2023-08-15 03:18:11 -0700

    add door status model

--- demo/models.py
+++ demo/models.py
@@ -100,3 +100,60 @@ def get_default_status():
     default_status, _ = OrderHistory.objects.get_or_create(status='pending')
     return default_status
 
+
+# store pick up door
+
+# door status : open | close | opening | closing | error | | package_ready | order_picked_up
+
+class DoorStatusHint(models.Model):
+    status = models.CharField(max_length=250, default="close")
+    status_id = models.IntegerField(default=1)
+    updated = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+# status : in_store | in location : AAAA_BBBB_2222 | picking_up from location ...| in robot hand | in cart to door
+# in door | in drone hand | in drone | in customer closer or location
+class PackageStatusHint(models.Model):
+    status = models.CharField(max_length=250, default="coming_to_door")
+    status_id = models.IntegerField(default="1")
+    updated = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class StoreDoorPickUp(models.Model):
+    address = models.CharField(max_length=200, default="36039 Pizarro dr, Fremont, Ca, 94653")
+    latitude = models.DecimalField(max_digits=20, decimal_places=7, default="37.5566056")
+    longitude = models.DecimalField(max_digits=20, decimal_places=7, default="-122.0287363")
+    order_description = models.TextField(default="no description")
+    description = models.TextField(default="no description")
+    madel = models.CharField(max_length=200, default="small_door_aaa_001")
+    square = models.CharField(max_length=200, default="20x20")
+    cube_size = models.CharField(max_length=200, default="20x20x20")
+    trigger = models.IntegerField(default=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=250, default="open")
+    status_id = models.SmallIntegerField(default=1)
+
+
+class DoorStatus(models.Model):
+    status = models.CharField(max_length=250, default="open")
+    status_id = models.SmallIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField(default="no description")
+    comment = models.TextField(default="no comment")
+    updated_from = models.CharField(max_length=200, default="programmer")
+    order_history = models.ForeignKey(StoreDoorPickUp, on_delete=models.CASCADE)
+
+
+class PackageStatus(models.Model):
+    status = models.CharField(max_length=250, default="coming_to_door")
+    status_id = models.IntegerField(default="1")
+    comment = models.TextField(default="no comment")
+    updated = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
